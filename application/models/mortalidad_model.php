79bb86a71082fa7b34203d3791073ee53dccb372
Guillermo Hernandez <0w3w4u@gmail.com>
2014-01-26 04:04:19 -0600

    API Mortalidad
    Signed-off-by: Guillermo Hernandez <0w3w4u@gmail.com>

---
+++ application/models/mortalidad_model.php
@@ -0,0 +1,31 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Mortalidad_model extends CI_Model {
+	function __construct() {
+		parent::__construct();
+	}
+	
+	/* Getters & Setters */
+	
+	function getMortalidad($edad="",$imc=""){
+		if(empty($edad) && empty($imc)){
+			return $this->db->select("idAfeccion")
+						->select("'' AS idImc", FALSE)
+						->select("'' AS idEdad", FALSE)
+						->select_sum("insidencias")
+						->from('mortalidad')
+						->group_by("idAfeccion")
+						->order_by("insidencias", "DESC")
+						->limit(10)
+						->get();
+		}else if(!empty($edad) && empty($imc)){
+			return NULL;
+		}else if(empty($edad) && !empty($imc)){
+			return NULL;
+		}else if(!empty($edad) && !empty($imc)){
+			return NULL;
+		}
+		return NULL;
+	}
+	
+}
